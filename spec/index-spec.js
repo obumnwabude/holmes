954d1a0985b888d90eec20aac2d71083e82120cc
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-05-06 17:17:22 +0100

    replace route testing tool from 'request' to 'supertest' because request has been deprecated

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,22 +1,17 @@
-const request = require('request');
-const server = require('../index');
-
-const baseUrl = process.env.HOST || 'http://localhost:3000/';
+const request = require('supertest');
+const app = require('../index');
 
 describe('Server', () => {
-  afterAll(() => server.close());
+  afterAll(() => app.close());
   describe('is working properly', () => {
     it('should return a status code of 200 on / (home route)', (done) => {
-      request.get(baseUrl, (error, response) => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-    });
-    it('should return OK on / (home route)', (done) => {
-      request.get(baseUrl, (error, response) => {
-        expect(response.body).toBe('OK');
-        done();
-      });
+      request(app)
+        .get('/')
+        .expect(200)
+        .end((err) => {
+          if (err) done.fail(err);
+          else done();
+        });
     });
   });
 });
