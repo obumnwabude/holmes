cf27fea9a5648d6f7980e0669ab1bbe480307c75
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-06-02 13:33:09 +0100

    feat: style scrollbars
    correct the mat-icon-button to import the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
