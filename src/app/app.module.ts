2b5d14d5fffc4f15871ce77d1e24d9d3b1424a3a
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-06-05 14:34:49 +0100

    get rid of sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,8 +17,6 @@ import { HomeComponent } from './home/home.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    MatListModule,
-    MatSidenavModule,
     MatToolbarModule,
   ],
   providers: [],
