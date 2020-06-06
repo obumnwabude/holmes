558e536a822d6ee088815fe4973448a22efb2a24
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-06-06 18:25:20 +0100

    feat(service): create an appService to provide streets and districts as observables
    update components and templates of streets and districts to use the service apprioprately

--- src/app/districts/districts.component.ts
+++ src/app/districts/districts.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
+import { AppService } from '../app.service';
 import { District } from './district.model';
 import { Street } from '../streets/street.model';
 
@@ -8,33 +11,13 @@ import { Street } from '../streets/street.model';
   styleUrls: ['./districts.component.scss'],
 })
 export class DistrictsComponent {
-  districts: District[] = [
-    { id: 1, name: 'Ilechukwu' },
-    { id: 2, name: 'Osonwa' },
-    { id: 3, name: 'Downtown' },
-    { id: 4, name: 'Ikegwuonu' },
-  ];
+  districts$: Observable<District[]> = this.service.getDistricts();
+  streets$: Observable<Street[]> = this.service.getStreets();
+  constructor(public service: AppService) {}
 
-  streets: Street[] = [
-    { id: 1, name: 'Udoji', district_id: 1, houses: 5 },
-    { id: 2, name: 'Umunkwuejim', district_id: 1, houses: 5 },
-    { id: 3, name: 'Anyijiofor', district_id: 1, houses: 5 },
-    { id: 4, name: 'Obikwulu', district_id: 1, houses: 5 },
-    { id: 5, name: 'Okafor', district_id: 2, houses: 5 },
-    { id: 6, name: 'Nwosu', district_id: 2, houses: 5 },
-    { id: 7, name: 'Obiegbunam', district_id: 2, houses: 5 },
-    { id: 8, name: 'Onyeaso', district_id: 2, houses: 5 },
-    { id: 9, name: 'Nweke', district_id: 3, houses: 5 },
-    { id: 10, name: 'Nwalo', district_id: 3, houses: 5 },
-    { id: 11, name: 'Achugonye', district_id: 3, houses: 5 },
-    { id: 12, name: 'Enyiokafor', district_id: 3, houses: 5 },
-    { id: 13, name: 'Enemuo', district_id: 4, houses: 5 },
-    { id: 14, name: 'Ogbodo', district_id: 4, houses: 5 },
-    { id: 15, name: 'Anozie', district_id: 4, houses: 5 },
-    { id: 16, name: 'Adindu', district_id: 4, houses: 5 },
-  ];
-
-  findStreets(id: number): Street[] {
-    return this.streets.filter((street) => street.district_id === id);
+  findStreets(id: number): Observable<Street[]> {
+    return this.streets$.pipe(
+      map((streets) => streets.filter((street) => street.district_id === id))
+    );
   }
 }
--- src/app/streets/streets.component.ts
+++ src/app/streets/streets.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
+import { AppService } from '../app.service';
 import { District } from '../districts/district.model';
 import { Street } from './street.model';
 
@@ -8,33 +11,13 @@ import { Street } from './street.model';
   styleUrls: ['./streets.component.scss'],
 })
 export class StreetsComponent {
-  districts: District[] = [
-    { id: 1, name: 'Ilechukwu' },
-    { id: 2, name: 'Osonwa' },
-    { id: 3, name: 'Downtown' },
-    { id: 4, name: 'Ikegwuonu' },
-  ];
+  streets$: Observable<Street[]> = this.service.getStreets();
+  districts$: Observable<District[]> = this.service.getDistricts();
+  constructor(public service: AppService) {}
 
-  streets: Street[] = [
-    { id: 1, name: 'Udoji', district_id: 1, houses: 5 },
-    { id: 2, name: 'Umunkwuejim', district_id: 1, houses: 5 },
-    { id: 3, name: 'Anyijiofor', district_id: 1, houses: 5 },
-    { id: 4, name: 'Obikwulu', district_id: 1, houses: 5 },
-    { id: 5, name: 'Okafor', district_id: 2, houses: 5 },
-    { id: 6, name: 'Nwosu', district_id: 2, houses: 5 },
-    { id: 7, name: 'Obiegbunam', district_id: 2, houses: 5 },
-    { id: 8, name: 'Onyeaso', district_id: 2, houses: 5 },
-    { id: 9, name: 'Nweke', district_id: 3, houses: 5 },
-    { id: 10, name: 'Nwalo', district_id: 3, houses: 5 },
-    { id: 11, name: 'Achugonye', district_id: 3, houses: 5 },
-    { id: 12, name: 'Enyiokafor', district_id: 3, houses: 5 },
-    { id: 13, name: 'Enemuo', district_id: 4, houses: 5 },
-    { id: 14, name: 'Ogbodo', district_id: 4, houses: 5 },
-    { id: 15, name: 'Anozie', district_id: 4, houses: 5 },
-    { id: 16, name: 'Adindu', district_id: 4, houses: 5 },
-  ];
-
-  findDistrict(id: number): District {
-    return this.districts.filter((district) => district.id === id)[0];
+  findDistrict(id: number): Observable<District> {
+    return this.districts$.pipe(
+      map((districts) => districts.filter((district) => district.id === id)[0])
+    );
   }
 }
